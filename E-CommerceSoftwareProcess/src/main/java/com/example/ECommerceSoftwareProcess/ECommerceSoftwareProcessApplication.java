f965dde5bf56d9e4734bc4b86c1a17f60a47d0dd
bigzaja4 <bigzaja4@hotmail.com>
2018-11-08 23:12:29 +0700

    init code spring

---
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/ECommerceSoftwareProcessApplication.java
@@ -0,0 +1,12 @@
+package com.example.ECommerceSoftwareProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSoftwareProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSoftwareProcessApplication.class, args);
+	}
+}
