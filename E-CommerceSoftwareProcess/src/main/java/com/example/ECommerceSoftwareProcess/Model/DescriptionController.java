01ff02d469ec6137c4f4e8a382c546bf0c53abf2
babyshun <tanita.ratanit@mail.kmutt.ac.th>
2018-11-09 20:41:19 +0700

    Create DescriptionController

---
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/DescriptionController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ECommerceSoftwareProcess.Model;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author itarataa
+ */
+@RestController
+public class DescriptionController {
+    
+    @Autowired
+    private DescriptionService descriptionService;
+    
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/descriptions"
+    )
+    
+    public Description insertDescription(@RequestBody @Valid Description description){
+        return descriptionService.insertDescription(description);
+    }
+    
+}
