2468d629e9b6cb84601ad20e3bc44f63153c70aa
bigzaja4 <bigzaja4@hotmail.com>
2018-11-09 22:39:59 +0700

    fix store bug //StoreController value same at ProductController

--- E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/Store.java
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/Store.java
@@ -1,98 +1,94 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.example.ECommerceSoftwareProcess.Model;
-//
-//import java.io.Serializable;
-//import java.util.List;
-//import javax.persistence.CascadeType;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.OneToMany;
-//import javax.persistence.OneToOne;
-//import javax.validation.constraints.NotNull;
-//
-///**
-// *
-// * @author Bank Puvadol
-// */
-//
-//@Entity
-////@Table(name = "Store")
-//public class Store implements Serializable{
-//    
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int storeId;
-////    @OneToOne
-////    @JoinColumn(table = "product" )
-//    private int productId;
-//    
-////    @NotNull
-////    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-////    private List<Product> products;
-//    private String storeName;
-//    private int follower;
-//    private int totalProduct;
-//
-//    public Store() {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ECommerceSoftwareProcess.Model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Bank Puvadol
+ */
+@Entity
+//@Table(name = "Store")
+public class Store implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int storeId;
+//    @OneToOne
+//    @JoinColumn(table = "product" )
+    private int productId;
+
+//    @NotNull
+//    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+//    private List<Product> products;
+    private String storeName;
+    private int follower;
+    private int totalProduct;
+
+    public Store() {
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+//    public List<Product> getProduct() {
+//        return this.products;
 //    }
 //
-//
-//    public int getStoreId() {
-//        return storeId;
+//    public void setProduct(List<Product> product) {
+//        this.products = product;
 //    }
-//
-//    public void setStoreId(int storeId) {
-//        this.storeId = storeId;
-//    }
-//
-//    public int getProductId() {
-//        return productId;
-//    }
-////    public List<Product> getProduct() {
-////        return this.products;
-////    }
-////
-////    public void setProduct(List<Product> product) {
-////        this.products = product;
-////    }
-//
-//    public String getStoreName() {
-//        return storeName;
-//    }
-//
-//    public void setStoreName(String storeName) {
-//        this.storeName = storeName;
-//    }
-//
-//    public int getFollower() {
-//        return follower;
-//    }
-//
-//    public void setFollower(int follower) {
-//        this.follower = follower;
-//    }
-//
-//    public int getTotalProduct() {
-//        return totalProduct;
-//    }
-//
-//    public void setTotalProduct(int totalProduct) {
-//        this.totalProduct = totalProduct;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Store{" + "storeId=" + storeId + ", productId=" + ", storeName=" + storeName + ", follower=" + follower + ", totalProduct=" + totalProduct + '}';
-//    }
-//    
-//    
-//    
-//}
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public int getFollower() {
+        return follower;
+    }
+
+    public void setFollower(int follower) {
+        this.follower = follower;
+    }
+
+    public int getTotalProduct() {
+        return totalProduct;
+    }
+
+    public void setTotalProduct(int totalProduct) {
+        this.totalProduct = totalProduct;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" + "storeId=" + storeId + ", productId=" + ", storeName=" + storeName + ", follower=" + follower + ", totalProduct=" + totalProduct + '}';
+    }
+
+}
--- E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/StoreController.java
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/StoreController.java
@@ -1,30 +1,30 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.example.ECommerceSoftwareProcess.Model;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// *
-// * @author Bank Puvadol
-// */
-//@RestController
-//public class StoreController {
-//    @Autowired
-//    private StoreService StoreServ;
-//
-//    @RequestMapping(
-//            method = RequestMethod.POST,
-//            value = "/productadd"
-//    )
-//    public Store insertProduct(@RequestBody Store st) {
-//        return StoreServ.insertStoreServ(st);
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ECommerceSoftwareProcess.Model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Bank Puvadol
+ */
+@RestController
+public class StoreController {
+    @Autowired
+    private StoreService storeService;
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/productaddtostore"
+    )
+    public Store insertProduct(@RequestBody Store st) {
+        return storeService.insertStoreServ(st);
+    }
+}
