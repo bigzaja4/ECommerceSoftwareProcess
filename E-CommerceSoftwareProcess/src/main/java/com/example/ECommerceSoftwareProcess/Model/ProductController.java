11b25d7ecbf659556d64ed05a6771b291ade8fe5
bigzaja4 <bigzaja4@hotmail.com>
2018-11-11 21:37:18 +0700

    Add Cross platform to ProductController

--- E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/ProductController.java
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/ProductController.java
@@ -8,6 +8,7 @@ package com.example.ECommerceSoftwareProcess.Model;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Bank Puvadol
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:8080",allowCredentials = "true")
 public class ProductController {
 
     @Autowired
