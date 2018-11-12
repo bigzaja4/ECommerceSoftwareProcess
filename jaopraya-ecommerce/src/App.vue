811ce0f3e0a13192f1040630c1f41fb6f4a4036a
NoeyKittiya <kittiya.yuthasastrkosol@mail.kmutt.ac.th>
2018-11-12 11:42:14 +0700

    edit homepage

--- jaopraya-ecommerce/src/App.vue
+++ jaopraya-ecommerce/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="homePage.css" rel="stylesheet" type="text/css" />
+    <link href="all.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
   
-    <div class="container">
-                <div class="row">
-                  <div class="col" style="background: #C44953; padding-right: 15px">
-                    <img src="./pic/logo.png" width="145px" height="100px"/>
+    <div class="container-fluid" style="background: #ECEDEF;">
+                <div class="row" style="">
+                  <div class="col" style="background: #C44953;">
+                    <img src="./pic/logo.png" width="145px" height="100px" />
                   </div>
                   <div class="col-6" style="background: #C44953; padding-top: 50px;text-align: left;">
                         <div class="input-group mb-3">
@@ -17,7 +18,7 @@
                               </div>
                   </div>
                   <div class="col" style="background: #C44953; padding-top: 5px;padding-right: 30px; text-align: right;">     
-                    <p class="nameUser" ><img src="./pic/user-avatar-main-picture.png" width="35px" height="35px"/>&nbsp;&nbsp;<b>Prayut</b></p>
+                    <p class="userName" ><img src="./pic/user-avatar-main-picture.png" width="35px" height="35px"/>&nbsp;&nbsp;<b>Prayut</b></p>
                     <div class="iconMenu">
                         <img src="./pic/shopping-cart.png" width="40px" height="40px"/>
                         <img src="./pic/notification.png" width="30px" height="30px"/>
@@ -25,8 +26,8 @@
                   </div>
                 </div>
                 <div id="nav">
-                  <router-link to="/">Home</router-link> |
-                  <router-link to="/about">About</router-link>
+                  <!-- <router-link to="/">Home</router-link> |
+                  <router-link to="/about">About</router-link> -->
                 </div>
                 <router-view/>
                 
@@ -56,4 +57,36 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+.userName{
+    color: white;
+    font-size: 17px;
+    padding-right: 20px;
+}
+.iconMenu{
+    margin-top: -10px;
+}
+.text1{
+    color: white;
+    font-size: 30px;
+    margin-top: -22px;
+    font-family: 'Kanit', sans-serif;
+}
+.card-text{
+    font-family: 'Kanit', sans-serif;
+    margin-top: -15px;
+}
+.card-title{
+    margin-top: -10px;
+}
+.sale{
+    background: #C44953;
+    color: #fff;
+    font-family: 'Kanit', sans-serif;
+    float: right;
+}
+.showPrice{
+    color: #C44953;
+    font-size: 20px; 
+}
+
 </style>
--- jaopraya-ecommerce/src/components/HelloWorld.vue
+++ jaopraya-ecommerce/src/components/HelloWorld.vue
@@ -1,56 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <div class="row text-center">
-        <div class="col" style="background: #ECEDEF; padding-right: 15px" v-for="item in product" :key="item">
-            <div class="col-lg-3">
-              <div class="card">
-                <img class="card-img-top" src="http://placehold.it/500x325" alt="productPicture">
-                <div class="card-body">
-                <h4 class="card-title">{{item.productName}}</h4>
-                <p class="card-text">{{item.productPrice}}&nbsp;THB&nbsp;<a class="btn btn-danger" href="#" role="button">Buy</a>
-                  <img src="../pic/shopping-cart1.png" width="30px" height="30px"/></p>
-                </div>
+  <!-- <div class="hello">
+    <h1>{{ msg }}</h1> -->
+  <div>
+    <div class="row text-center" style="padding:25px; margin-top:-55px">
+        <div class="col" v-for="item in product" :key="item">
+          <div class="card-deck">
+          <div class="card" style="width: 14rem;margin-bottom:15px">
+              <img class="card-img-top" src="http://placehold.it/500x325" alt="productPicture">
+              <div class="card-body">
+                 <h5 class="card-title" style="white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">{{item.productName}}</h5>
+                 <p class="card-text" >เสื้อแจ็คเก็ตวิ่งผู้หญิง</p>
+                 <p class="showPrice"><b>{{item.productPrice}}&nbsp;THB&nbsp;</b>
+                 <a class="btn sale" href="#" role="button">ซื้อเลย</a>
+                 <img src="../pic/shopping-cart1.png" width="20px" height="20px"/>
+                 </p>
               </div>
-            </div>
+          </div>
+          </div>
         </div>
-    </div>
+     </div>
   </div>
 
   
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 axios.defaults.withCredentials = true;
 export default {
   data() {
     return {
       product: {}
-    }
+    };
   },
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   },
   methods: {
-    isNextProduct: function(index){
-        let result = true;
-        if(product[index] == null){
-          result == false;
-        }
-        return result;
+    isNextProduct: function(index) {
+      let result = true;
+      if (product[index] == null) {
+        result == false;
+      }
+      return result;
     },
-    getAllProduct: async function(){
-        let product = await axios.get('http://localhost:8099/product');   
-        this.product = product.data;
-        console.log(this.product)
-     }
+    getAllProduct: async function() {
+      let product = await axios.get("http://localhost:8099/product");
+      this.product = product.data;
+      console.log(this.product);
+    }
   },
   mounted() {
     this.getAllProduct();
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -67,6 +71,7 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #ffffff;
 }
+
 </style>
