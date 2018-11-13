1ad09b9a3ec40ca5695b82a239a5f1cbbfaa01d2
NoeyKittiya <kittiya.yuthasastrkosol@mail.kmutt.ac.th>
2018-11-13 11:06:31 +0700

    login with facebook

--- E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/ECommerceSoftwareProcessApplication.java
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/ECommerceSoftwareProcessApplication.java
@@ -1,11 +1,35 @@
 package com.example.ECommerceSoftwareProcess;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class ECommerceSoftwareProcessApplication {
-
+@EnableOAuth2Sso
+@RestController
+public class ECommerceSoftwareProcessApplication extends WebSecurityConfigurerAdapter{
+    
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .csrf().disable()
+        .antMatcher("/**")
+        .authorizeRequests()
+          .antMatchers("/", "/login**", "/webjars/**")
+          .permitAll()
+        .anyRequest()
+          .authenticated()
+        .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+       return principal;
+}
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceSoftwareProcessApplication.class, args);
 	}
