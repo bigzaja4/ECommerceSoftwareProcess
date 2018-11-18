e8321f4ea2c41bdf8d2a3ef319a701d2e9da5c3e
Puvadol <puvadol_cc@hotmail.com>
2018-11-18 10:56:56 +0700

    create OrderProduct Model

---
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/OrderController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ECommerceSoftwareProcess.Model;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Bank Puvadol
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:8080",allowCredentials = "true")
+public class OrderController {
+    @Autowired
+    private OrderProductService orderService;
+    
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/order"
+    )
+    
+    public OrderProduct insertOrderService(@RequestBody @Valid OrderProduct Order){
+        return orderService.insertOrderProduct(Order);
+    }
+}
---
+++ E-CommerceSoftwareProcess/src/main/java/com/example/ECommerceSoftwareProcess/Model/OrderProduct.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ECommerceSoftwareProcess.Model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Bank Puvadol
+ */
+@Entity
+public class OrderProduct implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int OrderId;
+    private String address;
+    private Date orderDate;
+    private double totalPrice;
+    private String listProduct;
+    private String name;
+    private String tracking;
+
+    public OrderProduct() {
+    }
+    
+    
+
+    public int getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(int OrderId) {
+        this.OrderId = OrderId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDate() {
+        return orderDate;
+    }
+
+    public void setDate(Date date) {
+        this.orderDate = date;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(String listProduct) {
+        this.listProduct = listProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTracking() {
+        return tracking;
+    }
+
+    public void setTracking(String tracking) {
+        this.tracking = tracking;
+    }
+
+    
+    
+
+
+}
